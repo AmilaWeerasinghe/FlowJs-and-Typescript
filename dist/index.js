3089173079ba9b8c85e7c13b8aafd8e917ee5473
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2022-03-05 22:58:38 +0530

    interface functions and utility types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,26 @@
 // Variables
 let name = 'Amila Weerasinghe';
+let age = 25;
+let user = ["amila", "dilan"];
+let something = [1, 'amila']; //interface
+
+let person;
+person = {
+  name: "Amila",
+  age: 25,
+  hasSiblings: true
+};
+
+//functions
+function greeting(name) {
+  return 'Hello ' + name;
+}
+
+console.log(greeting('Amila'));
+
+function testFunction(value) {
+  return value;
+} //Utility types
+
+
+const clubs = 'Clubs'; // const Hearts:Suit = 'Hearts';
--- src/index.js
+++ src/index.js
@@ -3,3 +3,37 @@
 // Variables
 
 let name: string = 'Amila Weerasinghe';
+let age: number = 25;
+let user: string[] = ["amila" , "dilan" ];
+let something: any = [1 , 'amila'];
+
+//interface
+let person: Person;
+
+person = {
+    name: "Amila",
+    age: 25,
+    hasSiblings: true,
+}
+
+interface Person {
+    name: string;
+    age: number;
+    hasSiblings?: boolean;
+}
+
+//functions
+function greeting(name: string): string{
+    return 'Hello '+ name;
+}
+
+console.log(greeting('Amila'));
+
+function testFunction(value: string| number){
+    return value;
+}
+
+//Utility types
+type Suit = "Diamonds" | "Clubs" | "Spades";
+const clubs: Suit = 'Clubs';
+// const Hearts:Suit = 'Hearts';
